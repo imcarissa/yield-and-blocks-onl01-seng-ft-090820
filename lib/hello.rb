6777fcb75c5fe2b7c55d2e253442c6401ed97d9f
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-15 08:11:29 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,7 +1,12 @@
 def hello_t(array)
+  i = 0
+
+while i < array.length
+  yield array[1]
+  i = i +1
 
 hello_t(["Tim, "Tom", "Jim"]) do |name|
   if name.start_with?("T")
-      puts "Hi, #{name}"
-    end
+    puts "Hi, #{name}"
   end
+end
