825e504dacb5e1d26366f54610786f25c7522569
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-15 08:28:08 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,6 @@
 def hello_t(array)
-  i = 0
+  if block_given?
+    i = 0
 
   while i < array.length
     yield (array[i])
@@ -8,4 +9,7 @@ def hello_t(array)
 
 
   array
+else
+  puts "Hey! No block was given!"
+end
 end
